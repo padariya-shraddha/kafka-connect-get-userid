06963ecf6354d4fae583e6dff9748c95cbc53d3c
padariya-shraddha <padariya.shraddha@gmail.com>
2021-08-04 13:51:32 -0700

    first commit

---
+++ src/main/java/com/github/spadariya/kafka/connect/smt/ExtractUserid.java
@@ -0,0 +1,178 @@
+package com.github.spadariya.kafka.connect.smt;
+
+
+import org.apache.kafka.common.cache.Cache;
+import org.apache.kafka.common.cache.LRUCache;
+import org.apache.kafka.common.cache.SynchronizedCache;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SchemaUtil;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
+import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
+
+public abstract class ExtractUserid<R extends ConnectRecord<R>> implements Transformation<R> {
+
+    public static final String OVERVIEW_DOC =
+            "Insert a random UUID into a connect record";
+
+    private interface ConfigName {
+        String EXTRACT_FROM = "extract.from.field";
+        String EXTRACT_TO = "extract.to.field";
+    }
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(ConfigName.EXTRACT_FROM, ConfigDef.Type.STRING, "message", ConfigDef.Importance.HIGH,
+                    "Extract user id from this field")
+            .define(ConfigName.EXTRACT_TO, ConfigDef.Type.STRING, "user_id", ConfigDef.Importance.HIGH,
+                    "Extract user id to this field");
+
+    private static final String PURPOSE = "Extracting user id from record";
+
+    private String extractFromfieldName;
+    private String extractTofieldName;
+
+    private Cache<Schema, Schema> schemaUpdateCache;
+
+    @Override
+    public void configure(Map<String, ?> props) {
+        final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+        extractFromfieldName = config.getString(ConfigName.EXTRACT_FROM);
+        extractTofieldName = config.getString(ConfigName.EXTRACT_TO);
+
+        schemaUpdateCache = new SynchronizedCache<>(new LRUCache<Schema, Schema>(16));
+    }
+
+
+    @Override
+    public R apply(R record) {
+        if (operatingSchema(record) == null) {
+            return applySchemaless(record);
+        } else {
+            return applyWithSchema(record);
+        }
+    }
+
+    private R applySchemaless(R record) {
+        final Map<String, Object> value = requireMap(operatingValue(record), PURPOSE);
+
+        final Map<String, Object> updatedValue = new HashMap<>(value);
+
+        updatedValue.put(extractTofieldName, getUserid(value.get(extractFromfieldName)));
+
+        return newRecord(record, null, updatedValue);
+    }
+
+    private R applyWithSchema(R record) {
+        final Struct value = requireStruct(operatingValue(record), PURPOSE);
+
+        Schema updatedSchema = schemaUpdateCache.get(value.schema());
+        if(updatedSchema == null) {
+            updatedSchema = makeUpdatedSchema(value.schema());
+            schemaUpdateCache.put(value.schema(), updatedSchema);
+        }
+
+        final Struct updatedValue = new Struct(updatedSchema);
+
+        for (Field field : value.schema().fields()) {
+            updatedValue.put(field.name(), value.get(field));
+        }
+
+        updatedValue.put(extractTofieldName, getUserid(value.get(extractFromfieldName)));
+
+        return newRecord(record, updatedSchema, updatedValue);
+    }
+
+    @Override
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+
+    @Override
+    public void close() {
+        schemaUpdateCache = null;
+    }
+
+    private String getRandomUuid() {
+        return UUID.randomUUID().toString();
+    }
+
+    private long getUserid(Object message) {
+        String messageValue = String.valueOf(message);
+        //get userid from message
+        return 123l;
+    }
+
+    private Schema makeUpdatedSchema(Schema schema) {
+        final SchemaBuilder builder = SchemaUtil.copySchemaBasics(schema, SchemaBuilder.struct());
+
+        for (Field field: schema.fields()) {
+            builder.field(field.name(), field.schema());
+        }
+
+        builder.field(extractTofieldName, Schema.INT64_SCHEMA);
+
+        return builder.build();
+    }
+
+    protected abstract Schema operatingSchema(R record);
+
+    protected abstract Object operatingValue(R record);
+
+    protected abstract R newRecord(R record, Schema updatedSchema, Object updatedValue);
+
+    public static class Key<R extends ConnectRecord<R>> extends ExtractUserid<R> {
+
+        @Override
+        protected Schema operatingSchema(R record) {
+            return record.keySchema();
+        }
+
+        @Override
+        protected Object operatingValue(R record) {
+            return record.key();
+        }
+
+        @Override
+        protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+            return record.newRecord(record.topic(), record.kafkaPartition(), updatedSchema, updatedValue, record.valueSchema(), record.value(), record.timestamp());
+        }
+
+    }
+
+    public static class Value<R extends ConnectRecord<R>> extends ExtractUserid<R> {
+
+        @Override
+        protected Schema operatingSchema(R record) {
+            return record.valueSchema();
+        }
+
+        @Override
+        protected Object operatingValue(R record) {
+            return record.value();
+        }
+
+        @Override
+        protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+            return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), updatedSchema, updatedValue, record.timestamp());
+        }
+
+    }
+}
+
+
+
+
+// public class ExtractUserid { }
+
+
